6c2f3a3b5d9965bd160fd90f52846938df57af22
progre <djyayutto@gmail.com>
2021-08-26 22:34:04 +0900

    feat: wip br env

--- www/utils/env.ts
+++ www/utils/env.ts
@@ -1,10 +1,15 @@
 import fs from 'fs';
+import zlib from 'zlib';
 
 export const gcpCredentials: string | null =
-  process.env.GCP_CREDENTIALS ?? null;
+  zlib
+    .brotliDecompressSync(Buffer.from(process.env.GCP_CREDENTIALS_BR, 'base64'))
+    .toString() ?? null;
 export const rootServerOrigin =
   process.env.ROOT_SERVER_ORIGIN ?? 'http://admin:hoge@192.168.10.102';
 export const vercel = !!process.env.VERCEL;
 export const vercelEnv = process.env.VERCEL_ENV ?? 'local';
 export const ca =
-  process.env.CA_CERT_PEM ?? fs.readFileSync('secrets/ca.cert.pem');
+  zlib
+    .brotliDecompressSync(Buffer.from(process.env.CA_CERT_PEM_BR, 'base64'))
+    .toString() ?? fs.readFileSync('secrets/ca.cert.pem');
