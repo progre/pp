56ace53e488d6111df2f64d7d89a68e892e3b1af
progre <djyayutto@gmail.com>
2021-08-30 21:14:45 +0900

    chore: status更新

--- www/pages/_insecure/index.txt.ts
+++ www/pages/_insecure/index.txt.ts
@@ -14,10 +14,10 @@ function insecureHeader(): string {
       ip: '',
       url: 'https://p-at.net/index.txt',
       genre: '',
-      desc: '通信は暗号化されていません。',
+      desc: '通信は暗号化されていません',
       bandwidthType: '',
-      listeners: -1,
-      relays: -1,
+      listeners: -9,
+      relays: -9,
       bitrate: 0,
       type: 'RAW',
       track: {
