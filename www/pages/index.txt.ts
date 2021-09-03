27635897a3d0b707a8f5bd6a56f6e2f4cd28e1e2
progre <djyayutto@gmail.com>
2021-09-03 20:22:13 +0900

    refactor: performance improvement

--- www/pages/index.txt.ts
+++ www/pages/index.txt.ts
@@ -1,5 +1,5 @@
 import { GetServerSidePropsContext } from 'next';
-import handler from '../utils/handler';
+import { Readable } from 'stream';
 import { vercel } from '../utils/env';
 import { pageView } from '../utils/pageView';
 
@@ -13,8 +13,15 @@ export async function getServerSideProps({
   await pageView(req, resolvedUrl);
   const originURL = `${protocol}://${req.headers.host}/_internal/index.txt`;
   const originRes = await fetch(originURL);
-  const originText = await originRes.text();
-  handler(res, originText, false);
+  res.setHeader('Content-Type', originRes.headers.get('Content-Type') ?? '');
+  res.shouldKeepAlive = false;
+  res.writeHead(originRes.status);
+  const readable = originRes.body as unknown as Readable;
+  if (readable.addListener == null) throw new Error('node-fetch ではない');
+  for await (const buf of readable) {
+    res.write(buf);
+  }
+  res.end();
   return { props: {} };
 }
 
