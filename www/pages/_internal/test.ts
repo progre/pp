471c0baceb2d6805ef0815fbcf59ae3401fa5772
progre <djyayutto@gmail.com>
2021-10-01 00:54:02 +0900

    chore: add memory cache test

---
+++ www/pages/_internal/test.ts
@@ -0,0 +1,24 @@
+import { GetServerSidePropsContext } from 'next';
+import dummy from '../../utils/dummy';
+import handler from '../../utils/handler';
+
+let cachedTime = 0;
+let cachedIndexTxt: string | null = null;
+
+export async function getServerSideProps({
+  res,
+}: GetServerSidePropsContext): Promise<unknown> {
+  if (cachedIndexTxt != null && Date.now() < cachedTime + 60 * 1000) {
+    console.log('Cache hit');
+    return cachedIndexTxt;
+  }
+  const txt = dummy();
+  cachedTime = Date.now();
+  cachedIndexTxt = txt;
+  handler(res, txt, false);
+  return { props: {} };
+}
+
+export default function Null(): null {
+  return null;
+}
