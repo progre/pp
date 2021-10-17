b23163e766cf0e93adaffaa37542e63e30352043
progre <djyayutto@gmail.com>
2021-10-17 23:47:04 +0900

    fix: duplicate

--- www/pages/_internal/index.txt.ts
+++ www/pages/_internal/index.txt.ts
@@ -1,13 +1,11 @@
 import { GetServerSidePropsContext } from 'next';
 import generateIndexTxt from '../../utils/fetch/generateIndexTxt';
-import { info } from '../../utils/fetch/logger';
 import handler from '../../utils/http/handler';
 
 export async function getServerSideProps({
   res,
 }: GetServerSidePropsContext): Promise<unknown> {
   const txt = await generateIndexTxt();
-  info(txt);
   handler(res, txt, true);
   return { props: {} };
 }
