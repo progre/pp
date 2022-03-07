0632845cee7103e56163fd7fae1093b8532d9d07
progre <progre+pgp@prgrssv.net>
2022-03-07 09:28:58 +0900

    ci: add dev env

--- root/terraform.tf
+++ root/terraform.tf
@@ -16,6 +16,7 @@ variable "env_root_key_file_name" {}
 variable "google_cloud_dns_zone_name" {}
 variable "google_credential_file_name" {}
 variable "google_environment_target" {}
+variable "google_power" {}
 variable "google_project" {}
 
 provider "google" {
@@ -39,26 +40,34 @@ resource "google_compute_firewall" "tf_firewall" {
 }
 
 resource "google_compute_address" "tf_address" {
+  count = var.google_power ? 1 : 0
+
   name = "tf-${var.google_environment_target}-ipv4-address"
 }
 
 resource "google_dns_record_set" "resource_recordset" {
+  count = var.google_power && var.google_environment_target == "production" ? 1 : 0
+
   managed_zone = var.google_cloud_dns_zone_name
   name         = "${var.env_root_domain}."
   type         = "A"
-  rrdatas      = [google_compute_address.tf_address.address]
+  rrdatas      = [google_compute_address.tf_address[0].address]
   ttl          = 300
 }
 
 resource "google_dns_record_set" "resource_recordset2" {
+  count = var.google_power && var.google_environment_target == "production" ? 1 : 0
+
   managed_zone = var.google_cloud_dns_zone_name
   name         = "${var.env_insecure_domain}."
   type         = "A"
-  rrdatas      = [google_compute_address.tf_address.address]
+  rrdatas      = [google_compute_address.tf_address[0].address]
   ttl          = 300
 }
 
 resource "google_compute_instance" "tf_cloud_01" {
+  count = var.google_power ? 1 : 0
+
   name                      = "tf-${var.google_environment_target}"
   machine_type              = "e2-micro"
   zone                      = "us-west1-a"
@@ -102,7 +111,7 @@ resource "google_compute_instance" "tf_cloud_01" {
   network_interface {
     network = google_compute_network.tf_network.name
     access_config {
-      nat_ip = google_compute_address.tf_address.address
+      nat_ip = google_compute_address.tf_address[0].address
     }
   }
 }
--- scripts/extract-secrets.sh
+++ scripts/extract-secrets.sh
@@ -3,6 +3,7 @@
 cd root/ && \
 echo "$TF_TERRAFORM_TFVARS" > terraform.tfvars && \
 echo "google_environment_target = $GOOGLE_ENVIRONMENT_TARGET" >> terraform.tfvars && \
+echo "google_power = $GOOGLE_POWER" >> terraform.tfvars && \
 mkdir secrets/ && \
 echo "$TF_GOOGLE_CREDENTIAL_FILE" > secrets/google_credential.json && \
 echo "$TF_ROOT_CRT_FILE" > secrets/root.crt && \
