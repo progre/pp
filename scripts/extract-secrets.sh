13fac6107780dd80e0b474b3f4c372c04237282c
progre <progre+pgp@prgrssv.net>
2022-03-27 17:12:16 +0900

    fix: redirect

--- scripts/extract-secrets.sh
+++ scripts/extract-secrets.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -eux
 
-cd root/ && \
-echo "$TF_TERRAFORM_TFVARS" > terraform.tfvars && \
-echo "google_environment_target = \"$GOOGLE_ENVIRONMENT_TARGET\"" >> terraform.tfvars && \
-echo "google_power = \"$GOOGLE_POWER\"" >> terraform.tfvars && \
-mkdir secrets/ && \
-echo "$TF_GOOGLE_CREDENTIAL_FILE" > secrets/google_credential.json && \
-echo "$TF_ROOT_CRT_FILE" > secrets/root.crt && \
+cd root/
+echo "$TF_TERRAFORM_TFVARS" > terraform.tfvars
+echo "google_environment_target = \"$GOOGLE_ENVIRONMENT_TARGET\"" >> terraform.tfvars
+echo "google_power = \"$GOOGLE_POWER\"" >> terraform.tfvars
+mkdir secrets/
+echo "$TF_GOOGLE_CREDENTIAL_FILE" > secrets/google_credential.json
+echo "$TF_ROOT_CRT_FILE" > secrets/root.crt
 echo "$TF_ROOT_KEY_FILE" > secrets/root.key
