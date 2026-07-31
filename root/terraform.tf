77d933f25c054a13c74a49a2f6457e16b7577bc2
progre <1272114+progre@users.noreply.github.com>
2026-08-01 01:22:40 +0900

    chore: zone 変更

--- root/terraform.tf
+++ root/terraform.tf
@@ -18,7 +18,7 @@ variable "google_project" {}
 
 provider "google" {
   credentials = file("secrets/google_credential.json")
-  region      = "us-central1"
+  region      = "us-west1"
   project     = var.google_project
 }
 
@@ -47,7 +47,7 @@ resource "google_compute_instance" "tf_cloud_01" {
 
   name                      = "tf-${var.google_environment_target}"
   machine_type              = "e2-micro"
-  zone                      = "us-central1-f"
+  zone                      = "us-west1-b"
   tags                      = [google_compute_firewall.tf_firewall.name]
   allow_stopping_for_update = true
   boot_disk {
